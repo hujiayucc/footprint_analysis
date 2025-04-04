b2ac160daa9ccf37421c9536c29cd29df130221a
hujiayucc <hujiayucc@qq.com>
2025-04-04 22:12:39 +0800

    修复 日志输出提示GBK编码问题

--- src/evaluation.py
+++ src/evaluation.py
@@ -17,7 +17,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s] [%(levelname)s] %(message)s",
     handlers=[
-        logging.FileHandler("run.log"),
+        logging.FileHandler("run.log", encoding="utf-8"),
         logging.StreamHandler()
     ]
 )
